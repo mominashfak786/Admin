30bd7e3cfc9ea26e3f1280ce4ed4b49cbe3ddb70
mominashfak786 <ashfakmomin2001@gmail.com>
2023-06-01 23:05:19 +0530

    done

--- src/componenets/AdminDashboard.jsx
+++ src/componenets/AdminDashboard.jsx
@@ -31,8 +31,6 @@ const AdminDashboard = () => {
           </Link>
         </div>
 
-
-
         <div class=" p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-900">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Individual Dashboard
@@ -65,7 +63,7 @@ const AdminDashboard = () => {
           </h5>
 
           <Link
-            to="/"
+            to="/enterprisepass"
             class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Open
@@ -84,7 +82,7 @@ const AdminDashboard = () => {
             </svg>
           </Link>
         </div>
-       
+
         <div class=" p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-900">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             Service Team Dashboard
--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import LoginAdmin from "./componenets/loginadmin";
 import AdminDashboard from "./componenets/AdminDashboard";
 import Partner from "./routes/Partner";
 import Indivisual from "./routes/Indivisual";
+import Enterprise from "./routes/Enterprise";
 import Message from "./routes/message";
-import MessageInd from "./routes/messageIndivisual"
-import PartnerPass from "./Passwords/PartnerPass"
-import IndivisualPass from "./Passwords/IndivisualPass"
+import MessageInd from "./routes/messageIndivisual";
+import MessageEnt from "./routes/messageEnterprise";
+
+import PartnerPass from "./Passwords/PartnerPass";
+import IndivisualPass from "./Passwords/IndivisualPass";
+import EnterprisePass from "./Passwords/EnterprisePass";
 
 const App = () => {
   return (
@@ -20,11 +24,13 @@ const App = () => {
           <Route path="/admindashboard" element={<AdminDashboard />} />
           <Route path="/partnerpass" element={<PartnerPass />} />
           <Route path="/indivisualpass" element={<IndivisualPass />} />
+          <Route path="/enterprisepass" element={<EnterprisePass />} />
           <Route path="/partner" element={<Partner />} />
           <Route path="/indivisual" element={<Indivisual />} />
+          <Route path="/enterprise" element={<Enterprise />} />
           <Route path="/message" element={<Message />} />
           <Route path="/messageInd" element={<MessageInd />} />
-
+          <Route path="/messageEnt" element={<MessageEnt />} />
         </Routes>
       </BrowserRouter>
     </React.StrictMode>
